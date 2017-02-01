cba34fe5a3f86aafca81435aa7f4ceb31071e211
Jeff Dyer <jeffdyer@acm.org>
2017-02-01 16:21:57 -0700

    checkpoint

--- pub/lexicon.js
+++ pub/lexicon.js
@@ -1,27 +1,14 @@
 window.gcexports.globalLexicon = {
-    // keywords
-    "let" : { "tk": 0x12, "cls": "keyword" },
-    "if" : { "tk": 0x05, "cls": "keyword" },
-    "then" : { "tk": 0x06, "cls": "keyword" },
-    "else" : { "tk": 0x07, "cls": "keyword" },
-    "case" : { "tk": 0x0F, "cls": "keyword" },
-    "of" : { "tk": 0x10, "cls": "keyword" },
-    "end" : { "tk": 0x11, "cls": "keyword", "length": 0 },
-    "null" : { "tk": 0x15, "cls": "val", "length": 0 },
-    // booleans
-    "true" : { "tk": 0x14, "cls": "val", "length": 0 },
-    "false" : { "tk": 0x14, "cls": "val", "length": 0 },
-    // functions
-    "add" : { "tk": 0x01, "name": "ADD", "cls": "function", "length": 2 , "arity": 2 },    
-    "style" : { "tk": 0x01, "name": "STYLE", "cls": "function", "length": 2, "arity": 2 },
-    "type" : { "tk": 0x01, "name": "TYPE", "cls": "function", "length": 1, "arity": 1 },
-    "required" : { "tk": 0x01, "name": "REQUIRED", "cls": "function", "length": 1, "arity": 1 },
-    "metadata" : { "tk": 0x01, "name": "METADATA", "cls": "function", "length": 1, "arity": 1 },
-    "docs" : { "tk": 0x01, "name": "DOCS", "cls": "function", "length": 1, "arity": 1 },
-    "object" : { "tk": 0x01, "name": "OBJECT", "cls": "function", "length": 1, "arity": 1 },
-    "id" : { "tk": 0x01, "name": "ID", "cls": "function", "length": 1, "arity": 1 },
-    // number values
-    "ten" : { "tk": 0x01, "cls": "number", "val": "10", "length": 0 },
-    "USA" : { "tk": 0x01, "cls": "number", "val": "10001", "length": 0 },
-    "_" : { "tk": 0x01, "cls": "number", "val": "0", "length": 0 },
+    "null" : { "tk": 21, "cls": "val", "length": 0 },
+    "add" : { "tk": 1, "name": "ADD", "cls": "function", "length": 2 , "arity": 2 },    
+    "style" : { "tk": 1, "name": "STYLE", "cls": "function", "length": 2, "arity": 2 },
+    "type" : { "tk": 1, "name": "TYPE", "cls": "function", "length": 1, "arity": 1 },
+    "required" : { "tk": 1, "name": "REQUIRED", "cls": "function", "length": 1, "arity": 1 },
+    "metadata" : { "tk": 1, "name": "METADATA", "cls": "function", "length": 1, "arity": 1 },
+    "docs" : { "tk": 1, "name": "DOCS", "cls": "function", "length": 1, "arity": 1 },
+    "object" : { "tk": 1, "name": "OBJECT", "cls": "function", "length": 1, "arity": 1 },
+    "id" : { "tk": 1, "name": "ID", "cls": "function", "length": 1, "arity": 1 },
+    "ten" : { "tk": 1, "cls": "number", "val": "10", "length": 0 },
+    "USA" : { "tk": 1, "cls": "number", "val": "10001", "length": 0 },
+    "_" : { "tk": 1, "cls": "number", "val": "0", "length": 0 }
 }
