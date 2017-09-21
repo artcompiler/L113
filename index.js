4a76bd5cef108df5719e463c3a700b6b02822071
Jeff Dyer <jeffdyer@acm.org>
2017-09-21 11:45:38 -0700

    Enable auth and count

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
-/* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil; tab-width: 2 -*- */
-/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 /*
    L113 compiler service.
+   @flow weak
 */
-var fs = require('fs');
-var http = require('http');
-var express = require('express')
-var app = express();
-app.set('port', (process.env.PORT || 5113));
-app.set('views', __dirname);
+const https = require("https");
+const express = require('express')
+const compiler = require("./lib/compile.js");
+const app = express();
+const langID = "113";
+// SHARED START
+app.set('port', (process.env.PORT || "5" + langID));
 app.use(express.static(__dirname + '/pub'));
 app.get('/', function(req, res) {
-  res.send("Hello, L113!");
+  res.send("Hello, L" + langID + "!");
 });
-
-
-var compiler = require("./lib/compile.js");
-// Graffiti Code will load the version of itself that matches the graffiti
-// version. The compiler should use a version of itself that is compatible
-// with the language version. This version object is returned along with
-// the result of each /compile.
-var version = {
-  compiler: "v0.0.0",
-  language: "v0.0.0",
-  graffiti: "v0.0.0",
-};
-app.get('/version', function(req, res) {
-  res.send(version);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
 });
-app.get('/compile', function(req, res) {
-  var body = "";
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});
+app.get("/version", function(req, res) {
+  res.send(compiler.version || "v0.0.0");
+});
+app.get("/compile", function(req, res) {
+  let body = "";
   req.on("data", function (chunk) {
     body += chunk;
   });
   req.on('end', function () {
     body = JSON.parse(body);
-    let code = body.src;
-    let data = body.data;
-    let t0 = new Date;
-    var obj = compiler.compile(code, data, function (err, val) {
-      if (err.length) {
-        res.send({
-          error: err,
-        });
+    let auth = body.auth;
+    validate(auth, (err, data) => {
+      if (err) {
+        res.send(err);
       } else {
-        console.log("GET /compile " + (new Date - t0) + "ms");
-        res.send(val);
+        if (data.access.indexOf("compile") === -1) {
+          // Don't have compile access.
+          res.sendStatus(401).send(err);
+        } else {
+          let code = body.src;
+          let data = body.data;
+          let t0 = new Date;
+          let obj = compiler.compile(code, data, function (err, val) {
+            if (err.length) {
+              res.send({
+                error: err,
+              });
+            } else {
+              console.log("GET /compile " + (new Date - t0) + "ms");
+              res.json(val);
+            }
+          });
+        }
       }
     });
   });
@@ -54,56 +60,66 @@ app.get('/compile', function(req, res) {
     res.send(e);
   });
 });
-app.get('/view/:id', function(req, res) {
-  var id = req.params.id;
-  item(id, function (err, data) {
-    var obj = JSON.parse(data)[0].obj;
-    res.render('view.html', {
-      obj: JSON.stringify(obj),
-    }, function (error, html) {
-      if (error) {
-        console.log("error=" + error.stack);
-        res.send(400, error);
-      } else {
-        console.log("html=" + html);
-        res.send(html);
+function postAuth(path, data, resume) {
+  let encodedData = JSON.stringify(data);
+  var options = {
+    host: "auth.artcompiler.com",
+    port: "443",
+    path: path,
+    method: "POST",
+    headers: {
+      'Content-Type': 'text/plain',
+      'Content-Length': Buffer.byteLength(encodedData),
+    },
+  };
+  var req = https.request(options);
+  req.on("response", (res) => {
+    var data = "";
+    res.on('data', function (chunk) {
+      data += chunk;
+    }).on('end', function () {
+      try {
+        resume(null, JSON.parse(data));
+      } catch (e) {
+        console.log("ERROR " + data);
+        console.log(e.stack);
       }
+    }).on("error", function () {
+      console.log("error() status=" + res.statusCode + " data=" + data);
     });
-/*
-    if (err && err.length) {
-      res.send({
-        error: err,
-      });
-    } else {
-      res.send(obj);
-    }
-*/
   });
-  function item(id, resume) {
-    var options = {
-      method: "GET",
-      host: "www.graffiticode.com",
-      path: "/code/" + id,
-    };
-    var req = http.get(options, function(res) {
-      var data = "";
-      res.on('data', function (chunk) {
-        data += chunk;
-      }).on('end', function () {
-        try {
-          resume(null, data);
-        } catch (e) {
-          console.log("ERROR: " + e.stack);
-        }
-      }).on("error", function () {
-        resume("ERROR status=" + res.statusCode + " data=" + data, null);
-      });
+  req.end(encodedData);
+  req.on('error', function(err) {
+    console.log("ERROR " + err);
+    resume(err);
+  });
+}
+function count(token, count) {
+  postAuth("/count", {
+    jwt: token,
+    lang: "L" + langID,
+    count: count,
+  }, () => {});
+}
+const validated = {};
+function validate(token, resume) {
+  if (token === undefined) {
+    resume(null, {
+      address: "guest",
+      access: "compile",
+    });
+  } else if (validated[token]) {
+    resume(null, validated[token]);
+    count(token, 1);
+  } else {
+    postAuth("/validate", {
+      jwt: token,
+      lang: "L" + langID,
+    }, (err, data) => {
+      validated[token] = data;
+      resume(err, data);
+      count(token, 1);
     });
   }
-});
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-});
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
-});
+}
+// SHARED STOP
